acf8947fb8d76b54209af7bf773b3ee19b7283ff
izii <qiangqiangxuxu@hotmail.com>
2025-07-11 20:50:10 +0800

    Update ed25519.rs

--- src/crypto/sign/ed25519.rs
+++ src/crypto/sign/ed25519.rs
@@ -164,7 +164,7 @@ pub fn sign_detached(m: &[u8], sk: &SecretKey) -> Signature {
         );
     }
     assert_eq!(siglen, SIGNATUREBYTES as c_ulonglong);
-    Signature::new(sig)
+    Signature::from_bytes(sig)
 }
 
 /// `verify_detached()` verifies the signature in `sig` against the message `m`
@@ -220,7 +220,7 @@ impl State {
             );
         }
         assert_eq!(siglen, SIGNATUREBYTES as c_ulonglong);
-        Signature::new(sig)
+        Signature::from_bytes(sig)
     }
 
     /// `verify` verifies the signature in `sm` using the signer's public key `pk`.
@@ -336,7 +336,7 @@ mod test {
             let mut sig = sign_detached(&m, &sk).to_bytes();
             for j in 0..SIGNATUREBYTES {
                 sig[j] ^= 0x20;
-                assert!(!verify_detached(&Signature::new(sig), &m, &pk));
+                assert!(!verify_detached(&Signature::from_bytes(sig), &m, &pk));
                 sig[j] ^= 0x20;
             }
         }
